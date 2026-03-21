d02174bd9df430bb92bf476e1c2c026fb49aae4f
cordinantoine <cordin.antoine@gmail.com>
2026-03-21 20:24:17 +0100

    Create sport.js

---
+++ js/questions/sport.js
@@ -0,0 +1,18 @@
+/* ════════════════════════════════════════
+   questions/sport.js — Sport
+   ════════════════════════════════════════ */
+QUESTIONS.sport = [
+  {q:"Combien de joueurs y a-t-il sur le terrain par équipe au football ?",a:["9","10","11","12"],c:2,f:""},
+  {q:"Dans quel sport joue-t-on le tournoi de Wimbledon ?",a:["Tennis","Badminton","Squash","Ping-pong"],c:0,f:""},
+  {q:"Dans quel sport évolue Michael Jordan ?",a:["Baseball","Basket-ball","Football américain","Hockey"],c:1,f:""},
+  {q:"Dans quel sport utilise-t-on un ballon ovale ?",a:["Football","Rugby","Handball","Water-polo"],c:1,f:""},
+  {q:"Combien de points vaut un panier à trois points au basket-ball ?",a:["1","2","3","4"],c:2,f:""},
+  {q:"Combien de joueurs composent une équipe de basket-ball sur le terrain ?",a:["4","5","6","7"],c:1,f:""},
+  {q:"Dans quel sport est célèbre Usain Bolt ?",a:["Natation","Athlétisme","Cyclisme","Football"],c:1,f:""},
+  {q:"Combien de trous comporte un parcours classique de golf ?",a:["9","12","18","24"],c:2,f:""},
+  {q:"Dans quel sport pratique-t-on le Tour de France ?",a:["Marathon","Cyclisme","Triathlon","Ski"],c:1,f:""},
+  {q:"Dans quel sport est célèbre Roger Federer ?",a:["Tennis","Golf","Squash","Badminton"],c:0,f:""},
+  {q:"Dans quel sport marque-t-on un 'touchdown' ?",a:["Baseball","Football américain","Rugby","Handball"],c:1,f:""},
+  {q:"Dans quel sport est célèbre Lionel Messi ?",a:["Rugby","Football","Tennis","Basket"],c:1,f:""},
+  {q:"Quel pays a remporté la FIFA World Cup 2018 ?",a:["Allemagne","Brésil","France","Argentine"],c:2,f:""},
+];
